78e5e0cb0a2dacd4c4485a957ef1cc6d329d37e4
MunazzaZafarAlvi <munazzaalvi03@gmail.com>
2024-08-12 16:57:30 +0500

    Add Ransack

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,3 @@
 class HomeController < ApplicationController
   before_action :authenticate_customer!
-
-  def index
-    @q = Product.ransack(params[:q])
-    @products = @q.result(distinct: true)
-  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,18 @@
 class ProductsController < ApplicationController
   def index
+<<<<<<< Updated upstream
     @q = Product.ransack(params[:q])
     @products = @q.result(distinct: true) # This line should handle the search
 
     @ladies_products = @products.where(category: 'ladies') # Filter the searched results for ladies
     @gents_products = @products.where(category: 'gents')   # Filter the searched results for gents
+=======
+    if params[:category].present?
+      @products = Product.where(category: params[:category])
+    else
+      @products = Product.all
+    end
+>>>>>>> Stashed changes
   end
 
   def show
